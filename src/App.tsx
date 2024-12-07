9924adaf4883b4f075fed07da6ccaf81d2a7ec8d
Chiomadans <oluchukwu.alooba.com>
2024-12-07 12:06:31 +0100

    ch1_vd2_b

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import GlobalStyles from "./styles/globalStyles";
 
-const App = () => (
-  <ThemeProvider theme={theme}>
+const Home = () => <h1>Welcome to the Food App!</h1>;
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
     <GlobalStyles />
-    <div>App</div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+    </Routes>
   </ThemeProvider>
-);
+  );
+};
 
 export default App;
