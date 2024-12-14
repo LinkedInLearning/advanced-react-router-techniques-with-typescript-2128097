d1b4f38d4166fa635cce4663c55462ab672254e7
Chiomadans <oluchukwu.alooba.com>
2024-12-15 00:06:27 +0100

    ch4_vd3

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,5 @@
 import React, { lazy, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { ProtectedRoute } from "./PrivateRoutes";
 import { LoadingSpinner } from "../components/LoadingSpinner";
@@ -16,32 +17,36 @@ const Login = lazy(() => import("../pages/Login"));
 const Dashboard = lazy(() => import("../pages/Dashboard"));
 const Profile = lazy(() => import("../pages/Profile"));
 
+const FallbackUI = () => <h2>Something went wrong. Please try again later.</h2>;
+
 const AppRouter = () => {
   const location = useLocation();
 
   return (
-    <Suspense fallback={<LoadingSpinner size={60} color="#FF6347" speed={2} />}>
-      <Layout>
-        <Routes location={location}>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="menu" element={<Menu />}>
-            <Route path="spicy" element={<Spicy />}>
-              <Route path=":recipeId" element={<RecipeDetail />} />
+    <ErrorBoundary FallbackComponent={FallbackUI}>
+      <Suspense fallback={<LoadingSpinner size={60} color="#FF6347" speed={2} />}>
+        <Layout>
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="menu" element={<Menu />}>
+              <Route path="spicy" element={<Spicy />}>
+                <Route path=":recipeId" element={<RecipeDetail />} />
+              </Route>
+              <Route path="vegan" element={<Vegan />}>
+                <Route path=":recipeId" element={<RecipeDetail />} />
+              </Route>
+              <Route path="quick-easy" element={<QuickEasy />} />
+              <Route path="family-friendly" element={<FamilyFriendly />} />
+              <Route path="intercontinental" element={<Intercontinental />} />
             </Route>
-            <Route path="vegan" element={<Vegan />}>
-              <Route path=":recipeId" element={<RecipeDetail />} />
-            </Route>
-            <Route path="quick-easy" element={<QuickEasy />} />
-            <Route path="family-friendly" element={<FamilyFriendly />} />
-            <Route path="intercontinental" element={<Intercontinental />} />
-          </Route>
 
-          <Route path="dashboard" element={ <ProtectedRoute><Dashboard /></ProtectedRoute> }/>
-          <Route path="profile" element={ <ProtectedRoute><Profile /></ProtectedRoute> } />
-        </Routes>
-      </Layout>
-    </Suspense>
+            <Route path="dashboard" element={ <ProtectedRoute><Dashboard /></ProtectedRoute> }/>
+            <Route path="profile" element={ <ProtectedRoute><Profile /></ProtectedRoute> } />
+          </Routes>
+        </Layout>
+      </Suspense>
+    </ErrorBoundary>
   );
 };
 
